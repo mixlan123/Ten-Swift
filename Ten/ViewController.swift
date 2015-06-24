28a2a2176c70fa7ae8018cd0094620641cc605fb
海惠 <mo_cat@126.com>
2015-06-24 16:43:43 +0800

    Initial Commit

---
+++ Ten/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ten
+//
+//  Created by wuhh on 15/6/24.
+//  Copyright (c) 2015年 Ten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
